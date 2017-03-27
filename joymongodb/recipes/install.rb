a0b6f9f55ed002a373701947364af909df3af323
vensonjoy <valmeida@csc.com>
2017-03-27 20:36:37 +0530

    Update install.rb

--- joymongodb/recipes/install.rb
+++ joymongodb/recipes/install.rb
@@ -19,28 +19,32 @@
 # limitations under the License.
 #
 
-# Sets up the repositories for stable 10gen packages found here:
+# Sets up the repositories for stable mongodb-org packages found here:
 # http://www.mongodb.org/downloads#packages
+node.override['mongodb']['package_name'] = 'mongodb-org'
 
 case node['platform_family']
-when "debian"
+when 'debian'
   # Adds the repo: http://www.mongodb.org/display/DOCS/Ubuntu+and+Debian+packages
-  apt_repository "10gen" do
+  apt_repository 'mongodb' do
     uri "http://downloads-distro.mongodb.org/repo/#{node[:mongodb][:apt_repo]}"
-    distribution "dist"
-    components ["10gen"]
-    keyserver "hkp://keyserver.ubuntu.com:80"
-    key "7F0CEB10"
+    distribution 'dist'
+    components ['10gen']
+    keyserver 'hkp://keyserver.ubuntu.com:80'
+    key '7F0CEB10'
     action :add
   end
 
-when "rhel","fedora"
-  yum_repository "10gen" do
-    description "10gen RPM Repository"
-    url "https://repo.mongodb.org/yum/redhat/mongodb-org-3.1.repo#{node['kernel']['machine']  =~ /x86_64/ ? 'x86_64' : 'i686'}"
-    action :add
+when 'rhel', 'fedora'
+  yum_repository 'mongodb' do
+    description 'mongodb RPM Repository'
+    baseurl "http://downloads-distro.mongodb.org/repo/redhat/os/#{node['kernel']['machine']  =~ /x86_64/ ? 'x86_64' : 'i686'}"
+    action :create
+    gpgcheck false
+    enabled true
   end
 
 else
-    Chef::Log.warn("Adding the #{node['platform_family']} 10gen repository is not yet not supported by this cookbook")
+  # pssst build from source
+  Chef::Log.warn("Adding the #{node['platform_family']} 10gen repository is not yet not supported by this cookbook")
 end
